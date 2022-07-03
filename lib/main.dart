1bb9fff5534f51c3fc490f3495ece0e34a9891d8
“shibin-dev” <shibinflutter@gmail.com>
2022-07-03 22:25:50 +0530

    check remove

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
           canvasColor: canvasColor,
